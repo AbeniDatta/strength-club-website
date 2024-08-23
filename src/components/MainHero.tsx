17df8838aebbf80e33d2f299ad19afbc0db3cd38
claire-shen <claireshen1516@gmail.com>
2024-08-23 17:24:51 -0400

    finished more

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -16,21 +16,66 @@ const MainHero = () => {
         <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           {mainHero.description}
         </p>
-        <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-          <div className="rounded-md shadow">
+
+        <div className="mt-4 sm:mt-8 flex flex-wrap justify-center lg:justify-start gap-2">
+          {/* Instagram */}
+          <div className="rounded-md">
             <a
-              href={mainHero.primaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-yellow-500 hover:border-transparent hover:text-white md:py-4 md:text-lg md:px-10`}
+              href={mainHero.instagram.href}
+              className="flex items-center justify-center px-2 py-2 mt-1 border border-transparent rounded-md text-base font-medium hover:text-yellow-500 hover:border-transparent hover:border-transparent"
             >
-              {mainHero.primaryAction.text}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="w-16 h-16"
+                fill="currentColor"
+              >
+                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+              </svg>
             </a>
           </div>
-          <div className="mt-3 sm:mt-0 sm:ml-3">
+
+          {/* Discord */}
+          <div className="rounded-md">
             <a
-              href={mainHero.secondaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border-2 border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-gray-200 hover:border-transparent hover:text-black md:py-4 md:text-lg md:px-10`}
+              href={mainHero.discord.href}
+              className="flex items-center justify-center px-2 py-2 mt-1 border border-transparent rounded-md text-base font-medium hover:text-yellow-500 hover:border-transparent hover:border-transparent"
             >
-              {mainHero.secondaryAction.text}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                className="w-16 h-16"
+                fill="currentColor"
+              >
+                <path d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612" />
+              </svg>
+            </a>
+          </div>
+
+          {/* Linktree */}
+          <div className="rounded-md">
+            <a
+              href={mainHero.linktree.href}
+              className="flex items-center justify-center px-2 py-2 mt-1 border border-transparent rounded-md text-base font-medium hover:text-yellow-500 hover:border-transparent hover:border-transparent"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="w-14 h-14"
+                fill="currentColor"
+              >
+                <path d="M171.274 344.942h74.09v167.296h-74.09V344.942zM0 173.468h126.068l-89.622-85.44 49.591-50.985 85.439 87.829V0h74.086v124.872L331 37.243l49.552 50.785-89.58 85.24H417v70.502H290.252l90.183 87.629L331 381.192 208.519 258.11 86.037 381.192l-49.591-49.591 90.218-87.631H0v-70.502z" />
+              </svg>
+            </a>
+          </div>
+
+          {/* Email Button */}
+          <div className="sm:mt-0 sm:ml-3 flex justify-center">
+            <a
+              href={mainHero.email.href}
+              className={`w-full flex items-center justify-center mt-3 mb-3 px-8 py-3 border-2 border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-gray-200 hover:border-transparent hover:text-black md:py-4 md:text-lg md:px-10`}
+            >
+              {mainHero.email.text}
             </a>
           </div>
         </div>
